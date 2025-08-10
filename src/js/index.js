7d227a08e6bad45feb6242f31a297fdb4f65913a
Issa-Abbani <issaabbani04@gmail.com>
2025-08-10 18:57:29 +0300

    made a sidebar for mobile device navigation and coded all it's logic in the sidebar.js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
-import '../styles/styles.css';
+// import '../styles/styles.css';
+import { sidebar } from "./sidebar.js";
+
+
+function main(){
+  sidebar.sidebarEvents();
+}
+
+main();
+
 console.log('Hello world!');
