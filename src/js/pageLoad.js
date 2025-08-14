85c7a9fddd7ab62045d878afbb5dffd581c13940
Issa-Abbani <issaabbani04@gmail.com>
2025-08-14 15:29:21 +0300

    Made the end-home section dynamically generated via JS

--- src/js/pageLoad.js
+++ src/js/pageLoad.js
@@ -68,13 +68,56 @@ export const generatePage = (function(){
       aside.append(h1, p);
     }
 
+    function endHomeContent(){
+      const endHome = document.querySelector('section.end-home');
+      const endHomeItems = [
+            {
+              src: 'assets/svg/bakery_dining_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg',
+              text: 'The Best Breakfast'
+            },
+            {
+              src: 'assets/svg/local_cafe_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg',
+              text: 'Relxing Café'
+            },
+            {
+              src: 'assets/svg/dinner_dining_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg',
+              text: 'Various Cuisines'
+            },
+            {
+              src: 'assets/svg/kebab_dining_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg',
+              text: 'Traditional Arab Food'
+            },
+            {
+              src: 'assets/svg/award_meal_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg',
+              text: 'Award-Winning Food'
+            }
+          ];
+
+      endHomeItems.forEach(item =>{
+        //div container creation
+        const div = document.createElement('div');
+        div.classList.add('i-cont');
+        //children of div creation
+        const svg = document.createElement('img');
+        svg.src = item.src;
+        svg.loading = 'lazy';
+        svg.alt = 'not found';
+        
+        const span = document.createElement('span');
+        span.textContent = item.text;
+
+        //append items
+        endHome.append(div);
+        div.append(svg, span);
+      });
+    }
+
     generateSections();
     introHomeContent();
     midHomeContent();
+    endHomeContent();
 
   }//generateHome
-
-
 
 
   return{generateHome};
