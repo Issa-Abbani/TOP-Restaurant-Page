b4f017de2804ef67b60a42d6e476c83f5fbad256
Issa-Abbani <issaabbani04@gmail.com>
2025-08-14 20:48:46 +0300

    Updates

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../styles/styles.css';
-import { sidebar } from "./sidebar.js";
-import { tabSwitching } from "./tabSwitch.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-  sidebar.sidebarEvents();
-  tabSwitching();
-})
