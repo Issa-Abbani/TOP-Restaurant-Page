18ab6ff65030b5d614c3d1723ca68f9c102a2d7c
Issa-Abbani <issaabbani04@gmail.com>
2025-08-14 15:13:17 +0300

    Made the mid home page dynamically generated via JS

--- src/js/pageLoad.js
+++ src/js/pageLoad.js
@@ -44,8 +44,33 @@ export const generatePage = (function(){
       introHome.append(img, aside);
       aside.append(h1, p);
     }
+
+    function midHomeContent(){
+      const midHome = document.querySelector('section.home.mid-home');
+
+      //generate aside
+      const aside = document.createElement('aside');
+      //generate aside content
+      const h1 = document.createElement('h1');
+      h1.textContent = 'Comfortable Dining Area!';
+      
+      const p = document.createElement('p');
+      p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum distinctio odit consectetur hic ad repudiandae? Ex, natus odio fugiat voluptatum aliquid vero cumque, dicta quis, mollitia sapiente quae. Eos, officia? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla enim sed ipsa consequuntur esse autem iusto itaque repudiandae modi pariatur, laborum, inventore iste, minima architecto beatae numquam eius corporis sint.';
+
+      //generate image
+      const img = document.createElement('img');
+      img.src = 'assets/images/resto2_16by9.jpg';
+      img.alt = 'img not found';
+      img.loading = 'lazy';
+
+      //append content to section
+      midHome.append(aside, img);
+      aside.append(h1, p);
+    }
+
     generateSections();
     introHomeContent();
+    midHomeContent();
 
   }//generateHome
 
